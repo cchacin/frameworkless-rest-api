249623b2a40f8f6a382b563e7f2bb5e274a4617a
Carlos Chacin <cchacin@gmail.com>
2024-10-19 11:00:44 -0700

    Decoupling list of users with supplier

--- lib/src/main/java/org/acme/UsersServlet.java
+++ lib/src/main/java/org/acme/UsersServlet.java
@@ -9,6 +9,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 @WebServlet(value = "/users/*")
 public class UsersServlet extends HttpServlet {
@@ -21,20 +22,29 @@ public class UsersServlet extends HttpServlet {
     );
 
     private final Function<User, String> mapToJson;
+    private final Supplier<List<User>> getUsers;
 
-    public UsersServlet(Function<User, String> mapToJson) {
+    public UsersServlet(
+            Function<User, String> mapToJson,
+            Supplier<List<User>> getUsers
+    ) {
         this.mapToJson = mapToJson;
+        this.getUsers = getUsers;
     }
 
     public UsersServlet() {
-        this(UserJsonSerializer::serialize);
+        this(
+                UserJsonSerializer::serialize,
+                () -> USERS
+        );
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("application/json");
         response.setStatus(HttpServletResponse.SC_OK);
         response.getWriter().println(
-                USERS.stream()
+                getUsers.get()
+                        .stream()
                         .map(this.mapToJson)
                         .toList()
         );
