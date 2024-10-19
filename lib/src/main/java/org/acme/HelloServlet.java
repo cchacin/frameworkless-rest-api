5d0c567e99ba18d2dbdd1808f65775fd3060a52b
Carlos Chacin <cchacin@gmail.com>
2024-10-19 10:28:09 -0700

    Remove web.xml in favor of annotations

--- lib/src/main/java/org/acme/HelloServlet.java
+++ lib/src/main/java/org/acme/HelloServlet.java
@@ -1,12 +1,14 @@
 package org.acme;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
+@WebServlet(value = "/hello/*")
 public class HelloServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
