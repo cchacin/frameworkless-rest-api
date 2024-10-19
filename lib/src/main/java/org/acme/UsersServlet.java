c43b3a05a63e9dfc1963d4371053772b7acc7596
Carlos Chacin <cchacin@gmail.com>
2024-10-19 10:29:22 -0700

    Rename to UsersServlet

--- lib/src/main/java/org/acme/UsersServlet.java
+++ lib/src/main/java/org/acme/UsersServlet.java
@@ -8,8 +8,8 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebServlet(value = "/hello/*")
-public class HelloServlet extends HttpServlet {
+@WebServlet(value = "/users/*")
+public class UsersServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
         response.setStatus(HttpServletResponse.SC_OK);
